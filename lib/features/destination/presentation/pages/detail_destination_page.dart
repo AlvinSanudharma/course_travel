d30241c13e806cff39382a6fffea66f3169d925d
AlvinSanudharma <alvinsanudharma@gmail.com>
2024-07-27 22:45:11 +0800

    Photo gallery

--- lib/features/destination/presentation/pages/detail_destination_page.dart
+++ lib/features/destination/presentation/pages/detail_destination_page.dart
@@ -1,7 +1,11 @@
+import 'package:course_travel/api/urls.dart';
 import 'package:course_travel/features/destination/domain/entities/destination_entity.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:course_travel/features/destination/presentation/widgets/circle_loading.dart';
+import 'package:course_travel/features/destination/presentation/widgets/gallery_photo.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class DetailDestinationPage extends StatefulWidget {
   const DetailDestinationPage({super.key, required this.destination});
@@ -17,6 +21,107 @@ class _DetailDestinationPageState extends State<DetailDestinationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          gallery()
+        ],
+      ),
+    );
+  }
+
+  Widget gallery() {
+    List patternGallery = const [
+      StaggeredTile.count(3, 3),
+      StaggeredTile.count(2, 1.5),
+      StaggeredTile.count(2, 1.5)
+    ];
+
+    return StaggeredGridView.countBuilder(
+      itemCount: 3,
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      crossAxisCount: 5,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        if (index == 2) {
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return GalleryPhoto(
+                    images: widget.destination.images,
+                  );
+                },
+              );
+            },
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    itemGalleryImage(index),
+                    Container(
+                      color: Colors.black.withOpacity(0.3),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        '+ More',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    )
+                  ],
+                )),
+          );
+        }
+        return ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: itemGalleryImage(index));
+      },
+      staggeredTileBuilder: (index) {
+        return patternGallery[index % patternGallery.length];
+      },
+    );
+  }
+
+  Widget itemGalleryImage(int index) {
+    return ExtendedImage.network(
+      URLs.image(widget.destination.images[index]),
+      fit: BoxFit.cover,
+      handleLoadingProgress: true,
+      loadStateChanged: (state) {
+        if (state.extendedImageLoadState == LoadState.failed) {
+          return AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey[300],
+              child: const Icon(
+                Icons.broken_image,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }
+        if (state.extendedImageLoadState == LoadState.loading) {
+          return AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey[300],
+              child: const CircleLoading(),
+            ),
+          );
+        }
+        return null;
+      },
     );
   }
 
